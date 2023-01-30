5bf39448ba2dcb3043de0c3f5540d45d4de92c69
Claudio Blasco <claudioblascolm@gmail.com>
2023-01-30 12:49:48 -0300

    Primer escritura en el repositorio

---
+++ js/funciones.js
@@ -0,0 +1,23 @@
+
+document.getElementById("boton2").onclick = function () {
+    document.getElementById('boton2').style.display='none';
+    console.log ("capturamos el evento click");
+    document.getElementById("Francia").innerHTML = "Ragnar Lodbrok invadió Francia en el 845 d. C.";
+    
+} 
+
+
+
+document.getElementById("boton1").onclick = function () {
+    document.getElementById('boton1').style.display='none';
+    console.log ("capturamos el evento click");
+    document.getElementById("Inglaterra").innerHTML = "Ragnar Lodbrok invadió Inglaterra con el gran ejército pagano en el 865 d. C.";
+    
+}
+
+document.getElementById("boton").onclick = function () {
+    document.getElementById('boton').style.display='none';
+    console.log ("capturamos el evento click");
+    document.getElementById("Báltico").innerHTML = "Ragnar Lodbrok participó del asalto al Báltico junto a otros ejércitos vikingos en el 877 d. C.";
+
+} 
